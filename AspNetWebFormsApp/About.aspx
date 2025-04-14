1f085634b46f836f656b86b2b26c32a0fe8d9559
tornikeshavishvili <shavishvilitornike@gmail.com>
2025-04-14 21:26:44 +0400

    Initial Upload

---
+++ AspNetWebFormsApp/About.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="About" Theme="MyTheme2" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="AspNetWebFormsApp.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <main aria-labelledby="title">
+        <h2 id="title"><%: Title %>.</h2>
+        <h3>Your application description page.</h3>
+        <p>Use this area to provide additional information.</p>
+    </main>
+    <asp:Button runat="server" OnClientClick="onclientclick()" OnClick="Unnamed_Click"   SkinID="RedButton" />
+</asp:Content>
---
+++ AspNetWebFormsApp/About.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspNetWebFormsApp
+{
+    public partial class About : Page
+    { 
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            // Check if a theme is stored in session, else use a default value
+            if (Session["Theme"] != null)
+            {
+                Page.Theme = Session["Theme"].ToString();
+            }
+            else
+            {
+                // Optionally set a default theme here if none is chosen
+                Page.Theme = "MyTheme";
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+      
+        }
+
+        protected void Unnamed_Click(object sender, EventArgs e)
+        {
+            // Toggle between themes for demonstration
+            if (Session["Theme"] == null || Session["Theme"].ToString() == "MyTheme2")
+            {
+                Session["Theme"] = "MyTheme";
+            }
+            else
+            {
+                Session["Theme"] = "MyTheme2";
+            }
+            // Reload the page for the new theme to take effect
+            Response.Redirect(Request.Url.ToString());
+        }
+    }
+}
